ad9233f48c95fa54700200ca57c07922f6841081
Joe Chen <joseph.chen.od@gmail.com>
2019-07-28 22:26:24 -0500

    clean up

--- app/components/item-card.js
+++ app/components/item-card.js
@@ -1,9 +1,14 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
     isEditing: false,
     isFinished: false,
-    
+    publishedTime: computed('item.publishedOn', function() {
+        let cleanUp = this.item.publishedOn.toString().split(' ');
+        return `${cleanUp[0]} ${cleanUp[1]} ${cleanUp[2]} ${cleanUp[3]} at ${cleanUp[4]}`
+    }),
+
     actions: {
         updateItem(){
             this.updateItem();
